7c301bb9b8c26890937ec70781015713b9b34998
Maryucha <mym@certi.org.br>
2024-12-12 09:09:38 -0300

    fix: ajustes

--- localstack/localstack-setup.sh
+++ localstack/localstack-setup.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "Inicializando LocalStack e configurando serviços..."
 
+# Definir a região desejada
+REGION="us-east-1"
+
 # Configurar S3
-echo "Criando bucket S3..."
-awslocal s3 mb s3://mybucket --endpoint-url=http://localhost:4566
+echo "Criando bucket S3 na região $REGION..."
+awslocal s3 mb s3://mybucket --region $REGION --endpoint-url=http://localhost:4566
 
 # Configurar SQS
-echo "Criando fila SQS..."
-awslocal sqs create-queue --queue-name my-app-queue --endpoint-url=http://localhost:4566
+echo "Criando fila SQS na região $REGION..."
+awslocal sqs create-queue --queue-name my-app-queue --region $REGION --endpoint-url=http://localhost:4566
 
 # Configurar SES
-echo "Verificando identidade SES..."
-awslocal ses verify-email-identity --email-address app@demo.com --endpoint-url=http://localhost:4566
+echo "Verificando identidade SES na região $REGION..."
+awslocal ses verify-email-identity --email-address app@demo.com --region $REGION --endpoint-url=http://localhost:4566
 
 echo "Configuração concluída."
